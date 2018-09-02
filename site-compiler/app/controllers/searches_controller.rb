98ff3f05d57d86340cc8f0e63d097bc720126d90
Raiet <raietsaeed@gmail.com>
2018-09-02 20:50:29 +0000

    updated search controller

--- site-compiler/app/controllers/searches_controller.rb
+++ site-compiler/app/controllers/searches_controller.rb
@@ -1,12 +1,24 @@
 class SearchesController < ApplicationController
 
     def index
+        searches = Search.all
+        render json: searches
     end
     
     def show
+        search = Search.find(params[:id])
+        render json: search
     end
     
     def create
+        search = Search.create(search_params)
+        render json: search, status: 201
+    end
+
+    private
+    
+    def search_params
+        params.permit(:search_term, :created_at)
     end
 
 end
