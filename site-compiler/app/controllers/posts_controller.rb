a7af9d0e52f1a76bc8199168717c124e2cdf94b5
Raiet <raietsaeed@gmail.com>
2018-09-02 20:49:29 +0000

    updated post controller

--- site-compiler/app/controllers/posts_controller.rb
+++ site-compiler/app/controllers/posts_controller.rb
@@ -1,12 +1,24 @@
 class PostsController < ApplicationController
     
     def index
+        posts = Post.all
+        render json: posts
     end
     
     def show
+        post = Post.find(params[:id])
+        render json: post
     end
     
     def create
+        post = Post.create(post_params)
+        render json: post, status: 201
+    end
+
+    private
+    
+    def post_params
+        params.permit(:id, :site_id, :author, :title, :description, :url, :url_to_image, :published_at)
     end
 
 end
