667dd3cc6d51876ed072db049f5b8dfab2a69c7a
Raiet <raietsaeed@gmail.com>
2018-09-05 20:05:53 +0000

    separated Post.js into Expanded and Short versions

--- site-compiler/client/src/components/posts/Posts.js
+++ site-compiler/client/src/components/posts/Posts.js
@@ -1,2 +1,28 @@
 // Note : Presentational Component for showing all Post Objects (just so frame is being tracked)
 
+import React, { Component } from 'react';
+import ExpandedPost from './ExpandedPost';
+
+class Restaurants extends Component {
+
+  render() {
+    const { restaurants, deleteRestaurant } = this.props;
+    const restaurantList = restaurants.map(restaurant => {
+      return (
+        <Restaurant
+            key={restaurant.id}
+            restaurant={restaurant}
+            deleteRestaurant={deleteRestaurant}
+        />
+      )
+    });
+
+    return(
+      <ul>
+        {restaurantList}
+      </ul>
+    );
+  }
+};
+
+export default Restaurants;
--- site-compiler/client/src/components/posts/ShortPost.js
+++ site-compiler/client/src/components/posts/ShortPost.js
@@ -1,18 +1,16 @@
-// Note : Stateless Component for Post Object (just so frame is being tracked)
+// Note : Stateless Component for Short Post Object (just so frame is being tracked)
 
 import React from 'react'
  
-const Post = props => {
+const ShortPost = props => {
     return (
         <div>
             <img src={props.url_to_image} alt={props.url_to_image}></img>
             <li>Title : {props.title}</li>
             <li>Author : {props.author}</li>
-            <li>Description : {props.description}</li>
             <li>Published_at : {props.published_at}</li>
-            <li>Url : {props.url}</li>
         </div>
     );
 };
 
-export default Post;
+export default ShortPost;
