ac6870e042f4e149f4ad7dfceb46ddc25ca9ccb6
Raiet <raietsaeed@gmail.com>
2018-09-05 21:48:20 +0000

    moved site column info from main.js to sitecolumncontainer

--- site-compiler/client/src/containers/SiteColumnsContainer.js
+++ site-compiler/client/src/containers/SiteColumnsContainer.js
@@ -1,12 +1,14 @@
 // Note : Container for the SiteColumns (just so frame is being tracked)
 
 import React, { Component } from 'react'
+import Posts from '../components/posts/Posts'
 
 class SiteColumnsContainer extends Component {
 
   render() {
     return (
       <div>
+        <Posts posts={this.props.posts}/>
       </div>
     )
   }
--- site-compiler/client/src/routes/Main.js
+++ site-compiler/client/src/routes/Main.js
@@ -1,23 +1,16 @@
 // Note : This Route is the main page and should display the "main view" -- search/input Component + collapsible columns for the sites? + toggles for showing/hiding sites
 
 import React, { Component } from 'react';
-// import RestaurantInput from '../components/restaurants/RestaurantInput'
-import Posts from '../components/posts/Posts'
+import SiteColumnsContainer from '../containers/SiteColumnsContainer'
 import { connect } from 'react-redux'
 
 class Main extends Component {
-
-        // <RestaurantInput addRestaurant={this.props.addRestaurant}/>
-        // <Restaurants
-        //   restaurants={this.props.restaurants}
-        //   deleteRestaurant={this.props.deleteRestaurant}
-        // />
 
   render() {
     return (
       <div>
         <h3>Main View</h3>
-        <Posts posts={this.props.posts}/>
+        <SiteColumnsContainer posts={this.props.posts}/>
       </div>
     );
   }
@@ -28,10 +21,3 @@ class Main extends Component {
 const mapStateToProps = state => ({ posts: state.posts })
 
 export default connect(mapStateToProps)(Main)
-
-// const mapDispatchToProps = dispatch => ({
-//   addRestaurant: text => dispatch({type: 'ADD_RESTAURANT', text}),
-//   deleteRestaurant: id => dispatch({type: 'DELETE_RESTAURANT', id})
-// })
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Main)
