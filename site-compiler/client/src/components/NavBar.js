d4b4f7cbfd8e473ab92081f53fc99af88b4b4e23
Raiet <raietsaeed@gmail.com>
2018-09-05 21:18:51 +0000

    updated conflict between navbar css and short posts css but issue with width of navbar background

--- site-compiler/client/src/components/NavBar.js
+++ site-compiler/client/src/components/NavBar.js
@@ -4,14 +4,16 @@ import '../css/NavBar.css';
 
 const NavBar = () => {
     return (
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/main">Main</Link></li>
-            <li><Link to="/credits">Credits</Link></li>
-            <li><Link to="/showposts">ShowSitePosts</Link></li>
-            <li><Link to="/searchlist">SearchList</Link></li>
-            <li><Link to="/savedviews">SavedViews</Link></li>
-        </ul>
+        <div className="navbar">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/main">Main</Link></li>
+                <li><Link to="/credits">Credits</Link></li>
+                <li><Link to="/showposts">ShowSitePosts</Link></li>
+                <li><Link to="/searchlist">SearchList</Link></li>
+                <li><Link to="/savedviews">SavedViews</Link></li>
+            </ul>
+        </div>
     )
 }
 
