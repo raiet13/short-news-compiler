d236d9b73be5a189f8e7d19eb30e666d9b5f1168
Raiet <raietsaeed@gmail.com>
2018-09-05 20:16:12 +0000

    updated landing/main routes for clarity of expected functionality/UI

--- site-compiler/client/src/routes/LandingPage.js
+++ site-compiler/client/src/routes/LandingPage.js
@@ -1,4 +1,4 @@
-// Note : This Route is the main page and should display the "main view" -- search/input Component + collapsible columns for the sites? + toggles for showing/hiding sites
+// Note : This Route is Landing Page welcoming users to the app
 
 import React, { Component } from 'react';
 import logo from '../logo.svg';
