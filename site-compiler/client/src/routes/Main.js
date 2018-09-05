4c2a59db0767c905da2cd18f685c0cdf55277e9b
Raiet <raietsaeed@gmail.com>
2018-09-05 22:05:13 +0000

    updated textinput and main.js to handle textinput

--- site-compiler/client/src/routes/Main.js
+++ site-compiler/client/src/routes/Main.js
@@ -1,19 +1,48 @@
 // Note : This Route is the main page and should display the "main view" -- search/input Component + collapsible columns for the sites? + toggles for showing/hiding sites
 
 import React, { Component } from 'react';
-import SiteColumnsContainer from '../containers/SiteColumnsContainer'
 import { connect } from 'react-redux'
+import SiteColumnsContainer from '../containers/SiteColumnsContainer'
+import TextInput from '../components/TextInput'
 
 class Main extends Component {
-
-  render() {
-    return (
-      <div>
-        <h3>Main View</h3>
-        <SiteColumnsContainer posts={this.props.posts}/>
-      </div>
-    );
-  }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ""
+        };
+    }
+    
+    
+    // Submit term for searching
+    submitTerm(event) {
+      console.log('search : ', this.state.term);
+        this.setState({
+            term: ''
+        });
+    }
+    
+    // Handle onChange
+    handleSearchChange(event) {
+        console.log(event.target.value)
+        this.setState({
+            term: event.target.value
+        });
+    }
+    
+    render() {
+        return (
+          <div>
+            <h3>Main View</h3>
+            <form onSubmit={(event) => this.searchTerm(event)}>
+                <TextInput name="search" value={this.state.term} onChange={(event) => this.handleSearchChange(event)}/>
+                <button>Search</button>
+            </form>
+            <SiteColumnsContainer posts={this.props.posts}/>
+          </div>
+        );
+    }
 }
 
 
