261ec52eb6183af9d7554dfe11f39d922714a2ed
Raiet <raietsaeed@gmail.com>
2018-09-06 21:36:16 +0000

    working on sitecolumnscontainer function to map posts by site_id to unique arrays for individual posts components

--- site-compiler/client/src/containers/SiteColumnsContainer.js
+++ site-compiler/client/src/containers/SiteColumnsContainer.js
@@ -7,9 +7,39 @@ import Posts from '../components/posts/Posts'
 
 class SiteColumnsContainer extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            sitePosts: {}
+        };
+    }
+
+  // Function to map the posts to unique arrays based on site_id (to be fed into unique "Posts" components)
+  mapPostsBySite = () => {
+    console.log(this.props);
+    let postSiteID = 0;
+    let previousPostSiteID = 0;
+    let newSitePosts = [];
+
+    this.props.posts.map(post => {
+      console.log(postSiteID, previousPostSiteID);
+      postSiteID = post.site_id;
+      if (postSiteID === previousPostSiteID) {
+        newSitePosts << post;
+      } else {
+        // Add new site Posts
+        // this.setState({
+        //     sitePosts: { ...this.state.sitePosts, newSitePosts }
+        // });
+        newSitePosts = [];
+      }
+    })
+  }
+
   render() {
     return (
       <div>
+        {this.mapPostsBySite()}
         <Posts posts={this.props.posts}/>
       </div>
     )
