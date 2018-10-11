ef1a6f2bf9b2d97620fc32dcf49934f460be52e4
Raiet <raietsaeed@gmail.com>
2018-10-11 19:06:43 +0000

    updated to reflect creation of sites component, final use TBD once fetching wired up properly

--- site-compiler/client/src/routes/Credits.js
+++ site-compiler/client/src/routes/Credits.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
+import Sites from '../components/sites/Sites';
 
 class Credits extends Component {
   
@@ -9,7 +10,7 @@ class Credits extends Component {
     return(
       <div>
           <h3>Credits!</h3>
-          <p>Sites Used</p>
+          <p>Sites Used -- replace below with Sites component</p>
           <ul>
             <li><a href="https://www.washingtonpost.com/">The Washington Post</a></li>
             <li><a href="http://www.foxnews.com/">Fox News</a></li>
