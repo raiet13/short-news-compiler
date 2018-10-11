dbee0cf0cdc8aa322432254ae366bde04691557c
Raiet <raietsaeed@gmail.com>
2018-10-11 18:22:01 +0000

    added searches presentational component

--- site-compiler/client/src/components/searches/Searches.js
+++ site-compiler/client/src/components/searches/Searches.js
@@ -1,1 +1,33 @@
-// Note : Presentational Component for showing all Search Objects (just so frame is being tracked)
+// Note : Presentational Component for showing all Search Objects
+
+
+// NOTE : Next step = transforming list into a table
+/* 
+    https://stackoverflow.com/questions/39462458/react-js-creating-simple-table
+*/
+
+import React, { Component } from 'react';
+import Search from './Search';
+
+class Searches extends Component {
+
+  render() {
+    const { searches } = this.props;
+    const searchList = searches.map(search => {
+      return (
+        <Search
+            key={search.id}
+            search={search}
+        />
+      )
+    });
+
+    return(
+      <ul>
+        {searchList}
+      </ul>
+    );
+  }
+};
+
+export default Searches;
