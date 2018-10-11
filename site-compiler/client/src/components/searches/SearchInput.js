48fd8ae3b22e678152ff71da843b3a3a6411b8ea
Raiet <raietsaeed@gmail.com>
2018-10-11 18:12:50 +0000

    added searchinput presentational component

--- site-compiler/client/src/components/searches/SearchInput.js
+++ site-compiler/client/src/components/searches/SearchInput.js
@@ -1,1 +1,42 @@
 // Note : Component for creating new Searches (just so frame is being tracked)
+import React from 'react';
+
+class SearchInput extends React.Component {
+    
+    // Set up component's local state and functions
+    constructor(props) {
+        super(props);
+        this.state = {keyword: ''};
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    // Track the search param
+    handleChange(event) {
+        this.setState({
+            keyword: event.target.value
+        });
+    }
+    
+    // Submit the search and call the fetch requests
+    handleSubmit(event) {
+        alert('Search for: ' + this.state.keyword);
+        event.preventDefault();
+        // Get fetch requests
+    }
+    
+    // Display Input/Form
+    render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <label> Search for:
+                <input type="text" name="keyword" value={this.state.keyword} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Search" />
+          </form>
+        );
+    }
+}
+
+export default SearchInput;
