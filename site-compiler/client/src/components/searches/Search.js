57b6aa4dee5ffb7cf769abca54f5325fb44d4b0e
Raiet <raietsaeed@gmail.com>
2018-10-11 18:12:31 +0000

    added search object stateless component

--- site-compiler/client/src/components/searches/Search.js
+++ site-compiler/client/src/components/searches/Search.js
@@ -1,1 +1,18 @@
-// Note : Stateless Component for showing Search Object (just so frame is being tracked)
+// Note : Stateless Component for showing Search Object
+// Note : Figuring out how to handle "search sites" still
+
+import React from 'react';
+
+const Search = ({ search }) => {
+    return (
+        <div className="search clearfix">
+            <ul>
+                <li><strong>Keyword :</strong> {search.keyword}</li>
+                <li><strong>Sites Searched :</strong> {search.sites}</li>
+                <li><strong>Searched At :</strong> {search.created_at}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Search;
