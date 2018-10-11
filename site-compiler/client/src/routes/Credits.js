1b748a0c4f48853d2c491a7b097eee541d5bcfa7
Raiet <raietsaeed@gmail.com>
2018-10-11 18:43:13 +0000

    added basic frame for credits for static completion

--- site-compiler/client/src/routes/Credits.js
+++ site-compiler/client/src/routes/Credits.js
@@ -5,10 +5,20 @@ import React, { Component } from 'react';
 
 class Credits extends Component {
 
+  // Get list of sites used -- map to list items --> see below for frame
+  
+  
   render() {
     return(
       <div>
           <h3>Credits!</h3>
+          <p>Credit to NewsAPI</p>
+          <p>Sites Used</p>
+          <ul>
+            <li><a href="https://www.washingtonpost.com/">The Washington Post</a></li>
+            <li><a href="http://www.foxnews.com/">Fox News</a></li>
+            <li><a href="https://www.politico.com/">Politico</a></li>
+          </ul>
       </div>
     );
   }
