47fb5d94fe7718c04a7a82128393debaae3bfa17
Raiet <raietsaeed@gmail.com>
2018-10-18 00:30:12 +0000

    connected searchlist to store

--- site-compiler/client/src/routes/SearchList.js
+++ site-compiler/client/src/routes/SearchList.js
@@ -1,12 +1,28 @@
 // Note : This Route should display expanded information on all of the searches requested
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import Searches from '../components/searches/Searches';
 
 class SearchList extends Component {
   
+    constructor(props) {
+        super(props);
+        this.state = {
+            searches: []
+        };
+    }
+
   // Fetch Searches from Database
   // Display Searches
+  
+  // Map searches from store
+  mapSearches = (props) => {
+    console.log(this.props);
+    const allSearches = this.props.searches;
+    console.log(this.props.searches);
+    console.log(this.props.siteSearches);
+  }
   
   render() {
     return (
@@ -14,9 +30,13 @@ class SearchList extends Component {
           <h3>SearchList -- WIP</h3>
           <h3>All Completed Searches</h3>
           <div>SHOW_SEARCHES_COMPONENT</div>
+          <div>{this.mapSearches()}</div>
       </div>
     );
   }
 }
 
-export default SearchList;
+const mapStateToProps = store => ({ searches: store.searches, siteSearches: store.siteSearches })
+
+// export default SearchList;
+export default connect(mapStateToProps)(SearchList);
