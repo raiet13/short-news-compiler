788ca31e29fccc2af0797f42d89aca879412de1c
Raiet <raietsaeed@gmail.com>
2018-12-06 23:19:38 +0000

    added console log onClick for post id, but haven't figured out how to filter back to showsiteposts just yet

--- site-compiler/client/src/components/posts/ShortPost.js
+++ site-compiler/client/src/components/posts/ShortPost.js
@@ -6,12 +6,12 @@ import '../../css/ShortPost.css';
 const ShortPost = ({ post }) => {
     
     // Note : Just a little presentation updating 
-    if (post.author == '') { post.author = 'No Author Found'; }
-    // if (post.published_at == '') { post.published_at = 'Publish Date Not Found'; }
+    if (post.author === '') { post.author = 'No Author Found'; }
+    // if (post.published_at === '') { post.published_at = 'Publish Date Not Found'; }
     post.published_at = 'Publish Date Not Retrieved Properly yet -- WIP';
 
     return (
-        <div className="shortPost clearfix">
+        <div className="shortPost clearfix" onClick={() => (console.log(post.id))}>
             <ul>
                 <img src={post.url_to_image} alt={post.url_to_image}></img>
                 <li><strong>Title :</strong> {post.title}</li>
