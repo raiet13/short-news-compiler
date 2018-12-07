1221993e4e68baa0ebdfd92953c09bd79006a0ec
Raiet <raietsaeed@gmail.com>
2018-12-07 00:45:00 +0000

    updated site to include api name

--- site-compiler/client/src/components/sites/Site.js
+++ site-compiler/client/src/components/sites/Site.js
@@ -3,11 +3,13 @@
 import React from 'react';
 
 const Site = ({ site }) => {
+    console.log(`site info : ${site}`);
     return (
         <div className="site">
             <ul>
                 <li><strong>Name : </strong>{site.name}</li>
                 <li><strong>URL : </strong>{site.url}</li>
+                <li><strong>API Name : </strong>{site.api_name}</li>
                 <li><strong>API URL : </strong>{site.api_url}</li>
                 <li><strong>Added : </strong>{site.created_at}</li>
             </ul>
