752bb30a1722c1b5646f2d2f97dc60dd01d3938c
Raiet <raietsaeed@gmail.com>
2019-01-16 20:50:49 -0500

    temporarily removed WIP siteposts route for debugging ease

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import '../css/NavBar.css';
 
 const NavBar = () => {
+
+  // <li><Link to="/showposts">ShowSitePosts</Link></li>
     return (
         <div className="navbar">
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/showsitessearch">ShowSitesSearch</Link></li>
-                <li><Link to="/showposts">ShowSitePosts</Link></li>
                 <li><Link to="/searchlist">SearchList</Link></li>
                 <li><Link to="/credits">Credits</Link></li>
             </ul>
