3f01b8cf96e209075f345d753b2d3f875b8fa262
Raiet <raietsaeed@gmail.com>
2019-01-26 14:30:24 -0500

    updated search serializer and controller to streamline with new search-only db persistence

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,23 +1,23 @@
 class SearchesController < ApplicationController
 
-    # Sample Query = "curl localhost:8081/api/searches | jq '.'"
+    # Sample Query = "curl localhost:3001/api/searches | jq '.'"
     def index
         searches = Search.all
         render json: searches
     end
-    
+
     def show
         search = Search.find(params[:id])
         render json: search
     end
-    
+
     def create
         search = Search.create(search_params)
         render json: search, status: 201
     end
 
     private
-    
+
     def search_params
         params.permit(:search_term, :created_at)
     end
--- app/serializers/search_serializer.rb
+++ app/serializers/search_serializer.rb
@@ -1,5 +1,4 @@
 class SearchSerializer < ActiveModel::Serializer
   attributes :id, :search_term, :created_at
-  has_many :site_searches
 
 end
