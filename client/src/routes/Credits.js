34293de0f3c78d987f64d82d080e55a6f38fce91
Raiet <raietsaeed@gmail.com>
2019-01-26 14:24:43 -0500

    added titledescription to credits in prep for using component across all routes

--- client/src/routes/Credits.js
+++ client/src/routes/Credits.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 // import Sites from '../components/sites/Sites';
 
+// import TitleDescription from '../components/TitleDescription';
+
 class Credits extends Component {
 
   // Note : Replaces the hard-coded list of sites
