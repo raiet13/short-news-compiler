82d95f2edfbac32264dd62538ad727bdf5b639c9
Raiet <raietsaeed@gmail.com>
2019-01-26 15:52:36 -0500

    updated searchlist to remove unneeded noDispatch function and properly use props

--- client/src/routes/SearchList.js
+++ client/src/routes/SearchList.js
@@ -4,25 +4,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Searches from '../components/searches/Searches';
-import { fetchSearches, fetchSearchesNoDispatch } from '../actions/searches'
+import { fetchSearches } from '../actions/searches'
 import TitleDescription from '../components/TitleDescription';
 
 class SearchList extends Component {
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          searches: []
-      };
-  }
-
   componentDidMount() {
     console.log('Get Searches');
     this.props.fetchSearches();
-    // fetchSearchesNoDispatch();
   };
 
-  // <Searches searches={this.props.searches}/>
   render() {
 
     const searchListTitle = "All Completed Searches";
@@ -31,7 +22,7 @@ class SearchList extends Component {
     return (
       <div>
         <TitleDescription title={searchListTitle} description={searchListDescription} />
-        <Searches searches={this.state.searches}/>
+        <Searches searches={this.props.searches}/>
       </div>
     );
   };
