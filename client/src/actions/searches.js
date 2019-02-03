3d476847efb5f36999bfbb66ab29db6bffdeefac
Raiet <raietsaeed@gmail.com>
2019-02-03 12:44:26 -0500

    updated searches action based on project support meeting and notes to self

--- client/src/actions/searches.js
+++ client/src/actions/searches.js
@@ -1,20 +1,21 @@
 // Note : Actions for the Search Object
 
-// Add search to database
-export function addSearch(search) {
-  console.log(`Add search : ${search}`);
-  return dispatch => {
-    return fetch(`http://localhost:3001/api/searches`, {
-      method: 'post',
-      body: JSON.stringify({search_term: search}),
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }})
-    .then( response => {
-      // debugger;
-      console.log('posted new search to database : ', response)
-      dispatch({type: 'ADD_SEARCH', payload: search})
-    });
-  };
-};
+// Add search to database -- Function Mohammed at Project Support is working on
+// export function addSearch(search) {
+//   console.log(`Add search : ${search}`);
+//   return dispatch => {
+//     return fetch(`http://localhost:3001/api/searches`, {
+//       method: 'post',
+//       body: JSON.stringify({search_term: search}),
+//       headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }})
+//     .then( response => {
+//       debugger;
+//       console.log('posted new search to database : ', response)
+//       return response.json()
+//     })
+//     .then(data => dispatch({type: 'ADD_SEARCH', payload: search}));
+//   };
+// };
 
 
 // Fetch existing searches from database
@@ -27,5 +28,21 @@ export function fetchSearches() {
         type: 'FETCH_SEARCHES',
         payload: data
       }));
+  };
+};
+
+// Add search to database -- Note : Original not working add fetch -- check what is being pushed to the searches controller because it could have to do with the restrictions in the controller creation that are the problem
+export function addSearch(search) {
+  console.log(`Add search : ${search}`);
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/searches`, {
+      method: 'post',
+      body: JSON.stringify({search_term: search}),
+      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }})
+    .then( response => {
+      // debugger;
+      console.log('posted new search to database : ', response)
+      dispatch({type: 'ADD_SEARCH', payload: search})
+    });
   };
 };
